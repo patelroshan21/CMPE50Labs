b40482da5b5e5e304dd4c49499cd7c411d8e565a
Roshan <patelroshan21@yahoo.com>
2015-09-23 17:09:54 -0700

    Initial Commit

---
+++ CMPE50_Romel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CMPE50_Romel
+//
+//  Created by Roshan Patel on 9/23/15.
+//  Copyright © 2015 Roshan Patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
